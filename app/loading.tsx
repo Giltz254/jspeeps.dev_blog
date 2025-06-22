2dd9b2572e620dc3ef439d7c2628d0d8b83ea9b6
Giltz254 <kiptoogodffrey68@gmail.com>
2025-06-22 16:27:32 +0300

    loading

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,14 +1,12 @@
-import Loader from '@/components/custom/Loader'
-import React from 'react'
+import Loader from "@/components/custom/Loader";
+import React from "react";
 
 const loading = () => {
   return (
-    <div className='w-full bg-accent min-h-[calc(100vh-128px)]'>
-        <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 w-full flex items-center justify-center'>
-            <Loader />
-        </div>
+    <div className="w-full bg-accent min-h-[calc(100vh-128px)]">
+      <Loader />
     </div>
-  )
-}
+  );
+};
 
-export default loading
+export default loading;
